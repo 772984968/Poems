61d93655c69852e5348358635146d4697d0b1a55
maolin <maolin@4658482c-7cfe-9a96-3c50-ce57b7db7bff>
2020-03-13 13:29:36 +0000

    更新
    git-svn-id: https://github.com/772984968/Poems.git/trunk@4 4658482c-7cfe-9a96-3c50-ce57b7db7bff

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,16 +1,37 @@
 <?php
 
 namespace app\index\controller;
-
+use think\Db;
 class Index extends Base
 {
     public function Index()
     {
-
+        $posts=input('post.');
+        $type=input('get.type');
+     if (empty($type)) $type=1;
         $name = 'lisi';
         $this->assign('name', $name);
+        $types=Db::query('select * from b_type where 1');
+        foreach ($types as $key=>$vo){
+            if ($vo['id']==$type) $types[$key]['active']='hui-segment-active';
+
+        }
+        $list = Db::name('list')->where('type_id',$type)->paginate(2,false,
+            ['query'=>[
+                'type'=>$type
+            ]]);
+        $this->assign('list', $list);
+        $this->assign('type', $types);
         return $this->fetch();
     }
-
+    public function Detail()
+    {
+        $list_id=input('get.id');
+        $rst=Db::table('b_list')
+            ->where('id','=',$list_id)
+            ->find();
+        $this->assign('list', $rst);
+        return $this->fetch();
+    }
 
 }
