465d1f297ab53d6cfd37bf630f2c329111bf629b
maolin <772984968@qq.com>
2020-03-13 21:29:36 +0800

    更新

--- application/route.php
+++ application/route.php
@@ -5,3 +5,4 @@ Route::rule('login','index/user/login');
 Route::rule('register','index/user/register');
 Route::rule('user','index/user/index');
 Route::rule('logout','index/user/logout');
+Route::rule('detail','index/index/detail');
