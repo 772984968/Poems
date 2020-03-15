04f03007b702a744f99a71059dca91fd586e59e2
maolin <772984968@qq.com>
2020-03-15 20:09:57 +0800

    更新

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -16,7 +16,10 @@ class Index extends Base
             if ($vo['id']==$type) $types[$key]['active']='hui-segment-active';
 
         }
-        $list = Db::name('list')->where('type_id',$type)->paginate(10,false,
+        $list = Db::name('list')->where([
+            'type_id'=>$type,
+            'status'=>1
+        ])->paginate(10,false,
             ['query'=>[
                 'type'=>$type
             ]]);
@@ -30,7 +33,18 @@ class Index extends Base
         $rst=Db::table('b_list')
             ->where('id','=',$list_id)
             ->find();
+        $user=db('user')->where([
+            'id'=>$rst['uid']
+        ])->find();
+
+        $comments=db('comments')->where([
+            'uid'=>session('uid'),
+            'list_id'=>$list_id,
+            'status'=>1,
+        ])->select();
+
         $this->assign('list', $rst);
+        $this->assign('user', $user);
         return $this->fetch();
     }
     public function Create()
@@ -42,6 +56,9 @@ class Index extends Base
                 '2'=>'宋朝',
                 '3'=>'其他',
             ];
+            $src='/img/img/'.random_int(1,10).'.jpg';
+            $user=db('user')->where('id',session('uid'))->find();
+            $status=!empty($user['is_admin'])?1:0;
             $rst=db('list')->insertGetId([
                 'type_id'=>$post['type'],
                 'biaoti'=>$post['biaoti'],
@@ -51,7 +68,8 @@ class Index extends Base
                 'comment'=>!empty($post['comment'])?$post['comment']:'',
                 'introduction'=>!empty($post['introduction'])?$post['introduction']:'',
                 'date'=>date('Y-m-d H:i:s'),
-                'src'=>'../img/medialist/1.jpg',
+                'src'=>$src,
+                'status'=>$status,
                 'uid'=>session('uid'),
             ]);
             if (!empty($rst)){
